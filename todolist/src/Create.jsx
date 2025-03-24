f7101374a67f17f47640f643be495f745eae4e05
Dilshan Randika Sandaruwan <randikasandaruwan917@gmail.com>
2025-03-24 20:37:42 +0530

    UI styles

---
+++ todolist/src/Create.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Create = () => {
+  return (
+    <>
+        <input type="text" name='' id='' className='input-field' />
+        <button type='button' className='button'>Add</button>
+    </>
+  )
+}
+
+export default Create;
---
+++ todolist/src/Home.jsx
@@ -0,0 +1,32 @@
+import React , {useState} from 'react'
+import Create from './Create'
+
+function Home() {
+    const [todos, setTodos] = useState(["assdd","asdasd","","mn mn","kjkjnkj"]);
+  return (
+    <>
+    <div className="container">
+    <div className='todo-box'>
+        <h1>To-Do ListApp</h1>
+        <Create/>  
+    </div>
+
+    <div className="task">
+        {
+            todos.length === 0
+            ?
+            <p>No tasks</p>
+            :
+            todos.map(todo=>(
+                <div className='task-item'>
+                    <h3>{todo}</h3>
+                </div>
+            ))         
+        }
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Home;
