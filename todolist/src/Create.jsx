9af9bf3871fb0ca8a0fcc7edbe83db91f8fbcbbb
Dilshan Randika Sandaruwan <randikasandaruwan917@gmail.com>
2025-03-25 21:44:58 +0530

    mostly done

--- todolist/src/Create.jsx
+++ todolist/src/Create.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Create = () => {
+
+  const [task, setTask]=useState()
+
+  const handleAdd = ()=> {
+    axios.post('http://localhost:3001/add', {task:task})
+    .then(res=> {
+      location.reload()  /* Automatically Reload after task add */
+    })
+    .catch(err=> {
+      console.log(err)
+    })
+  }
+
   return (
     <>
-        <input type="text" name='' id='' className='input-field' />
-        <button type='button' className='button'>Add</button>
+        <input type="text" placeholder='Enter Task' className='input-field' onChange={(e)=>setTask(e.target.value)} />
+        <button type='button' className='button' onClick={handleAdd}>Add</button>
     </>
   )
 }
