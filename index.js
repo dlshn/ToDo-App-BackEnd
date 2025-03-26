a22be7f07a37dbc6580c2089e4e02e399d33abb2
Dilshan Randika Sandaruwan <randikasandaruwan917@gmail.com>
2025-03-26 19:22:26 +0530

    Mongodb Atlas connected

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const TodoModel = require('./Models/Todo');
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017/Todo', )
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.get('/get',(req,res) =>{
     TodoModel.find()  /* all records get */
@@ -38,6 +44,6 @@ app.post('/add',(req,res) =>{
     .catch(err=> res.json(err))
 })
 
-app.listen(3001, ()=>{
-    console.log("Server is running!")
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
